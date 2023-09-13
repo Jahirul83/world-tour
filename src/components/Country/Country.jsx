20b6594e9a0fc1572addd566d3d3910cd39f8a47
Jahirul83 <jahirulislamcse83.com>
2023-09-14 04:16:29 +0600

    manipulating components states

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import './Country.css'
 const Country = ({country}) => {
     console.log(country);
     const {name,flags,population,area,cca3} = country;
+
+    const [visited,setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
     return (
         <div className='country'>
             <h3>Name: {name?.common}</h3>
@@ -9,6 +16,10 @@ const Country = ({country}) => {
             <p>Population: {population}</p>
             <p>Area: {area}</p>
             <p>Code: {cca3}</p>
+            <button onClick={handleVisited}>{visited?'Visited':'not visited'}</button>
+            {
+                visited ?'I visited this country' : 'I want to visit'
+            }
         </div>
     );
 };
